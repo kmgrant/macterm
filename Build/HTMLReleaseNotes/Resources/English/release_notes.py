4ffef0b17c02e982ef4ff6091b5817a96c2e40f3
Kevin Grant <kmg@mac.com>
2008-03-03 06:04:20 +0000

    Minor fix.

--- Build/HTMLReleaseNotes/Resources/English/release_notes.py
+++ Build/HTMLReleaseNotes/Resources/English/release_notes.py
@@ -106,7 +106,7 @@ notes_by_version = {
 		'A warning is now displayed in the Format pane if the user chooses a font that will be slow.',
 		'Terminal inactivity notification now supports an additional reaction, "keep alive", which sends text to the server after 10 minutes.',
 		'Added Session.set_keep_alive_transmission(str) to Quills, allowing Python to override what is sent after a keep-alive timer expires.',
-		'Added Session.keep_alive_transmission(str) to Quills, to determine what string is sent to sessions when keep-alive timers expire.',
+		'Added Session.keep_alive_transmission() to Quills, to determine what string is sent to sessions when keep-alive timers expire.',
 	],
 	'20080301': [
 		'Preferences window Formats pane is now as wide as many other panels, which avoids some resizing and truncation of toolbar icons.',
