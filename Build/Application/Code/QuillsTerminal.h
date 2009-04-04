1697950ad809800aad6e0ebea2c20e9756923602
Kevin Grant <kmg@mac.com>
2009-04-04 03:43:31 +0000

    Removed unused BasicPalette class. Corrected documentation for set_dumb_string_for_char().

--- Build/Application/Code/QuillsTerminal.h
+++ Build/Application/Code/QuillsTerminal.h
@@ -57,58 +57,17 @@ public:
 #if SWIG
 %feature("docstring",
 "Specifies an appropriate dumb-terminal rendering for the given\n\
-character code.\n\
+character code, which is UTF-16 in the range 0-0xFFFF.  This is\n\
+global to all terminal views that are using the DUMB emulator,\n\
+it cannot be set on a per-screen basis.\n\
 \n\
-Normally, you should just call dumb_strings_init() to register\n\
-a callback that is automatically consulted for each character\n\
-code in turn.  If dumb_strings_init() is called after this\n\
-function, your changes will be overridden by the callback; but\n\
-you can call this routine after registering a callback to make\n\
-minor corrections.\n\
+The dumb terminal has a default rendering for codes that have\n\
+not specified a different rendering, and the default usually\n\
+just prints the numerical value in angle brackets.\n\
 ") set_dumb_string_for_char;
 #endif
 	static void set_dumb_string_for_char	(unsigned short		unicode,
 											 std::string		rendering_utf8);
-};
-
-class BasicPalette
-{
-public:
-	BasicPalette ();
-	
-#if SWIG
-%feature("docstring",
-"Return the red, green and blue components of the specified\n\
-color in the palette.  Note that Python already has utilities\n\
-for dealing with colors (the 'colorsys' conversion routines,\n\
-for instance), so MacTelnet does not provide much here.\n\
-\n\
-Currently, the color can be one of: 'red', 'green', 'yellow',\n\
-'blue', 'magenta', 'cyan', 'black' or 'white'.\n\
-") rgb;
-
-%exception rgb {
-	try {
-		$action
-	} catch (std::invalid_argument const& e) {
-		PyErr_SetString(PyExc_KeyError, e.what());
-		return NULL;
-	}
-}
-#endif
-	std::vector< double > rgb (std::string	inColor);
-
-private:
-	// currently, RGB color space is assumed internally; in the future
-	// it may be necessary to have a variable that holds the color space
-	std::vector< double >	_red;
-	std::vector< double >	_green;
-	std::vector< double >	_yellow;
-	std::vector< double >	_blue;
-	std::vector< double >	_magenta;
-	std::vector< double >	_cyan;
-	std::vector< double >	_white;
-	std::vector< double >	_black;
 };
 
 } // namespace Quills
