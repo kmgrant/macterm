5f756e7784e8d9c2d10275811718098385650e2c
Kevin Grant <kmg@mac.com>
2010-10-29 03:00:41 +0000

    Updated the project version of SWIG to 2.0.1.

--- Build/Tools/MacTelnetSwigConfig.sh
+++ Build/Tools/MacTelnetSwigConfig.sh
@@ -6,7 +6,16 @@
 # (Download SWIG from "http://www.swig.org/".)
 #
 # Run this instead of "./configure" from your untarred
-# "swig-<x.x>" build directory.
+# "swig-<x.x>" build directory.  Then run "make",
+# "make check" and "make install" to install SWIG.
+# (Mac OS X comes with an older version of SWIG in
+# /usr/bin, but that version has not been tested;
+# maybe it works, maybe not.)
+#
+# Note the choice of /opt/pcre below as the location
+# of PCRE; you could probably use "--without-pcre" if
+# you don't have it installed, because MacTelnet does
+# not currently rely on PCRE support in SWIG.
 #
 # Kevin Grant (kmg@mac.com)
 # July 29, 2006
@@ -17,7 +26,7 @@
 #
 # SWIG 1.3.29 or later works with Tiger (10.4).
 # SWIG 1.3.30 or later is required for Leopard (10.5).
-swigversion=1.3.39
+swigversion=2.0.1
 prefix_with_ver=/opt/swig-${swigversion}
 prefix_no_ver=/opt/swig
 
@@ -28,6 +37,7 @@ ln -snf ${prefix_with_ver} ${prefix_no_ver}
 # enable only Python
 ./configure \
 --prefix=${prefix_with_ver} \
+--with-pcre-prefix=/opt/pcre \
 --disable-ccache \
 --with-python \
 --without-allegrocl \
@@ -35,6 +45,7 @@ ln -snf ${prefix_with_ver} ${prefix_no_ver}
 --without-clisp \
 --without-csharp \
 --without-gcj \
+--without-go \
 --without-guile \
 --without-java \
 --without-lua \
