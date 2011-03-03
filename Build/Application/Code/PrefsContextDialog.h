3c0e10f52303c44fb1ff4cfe428592ce0ed6afdb
Kevin Grant <kmg@mac.com>
2011-03-03 01:04:14 +0000

    Fixed an incorrect comment.

--- Build/Application/Code/PrefsContextDialog.h
+++ Build/Application/Code/PrefsContextDialog.h
@@ -11,7 +11,7 @@
 /*###############################################################
 
 	MacTelnet
-		© 1998-2009 by Kevin Grant.
+		© 1998-2011 by Kevin Grant.
 		© 2001-2003 by Ian Anderson.
 		© 1986-1994 University of Illinois Board of Trustees
 		(see About box for full list of U of I contributors).
@@ -55,8 +55,7 @@
 #pragma mark Constants
 
 /*!
-Which characters are used for line endings in text files
-(such as file captures and saved selections).
+Options to control the contents of the dialog.
 */
 enum PrefsContextDialog_DisplayOptions
 {
