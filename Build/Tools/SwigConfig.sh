d95323230f5639dd4c3b6e2e8e1785e8f6e64fed
Kevin Grant <kmg@mac.com>
2015-02-09 01:48:34 +0000

    Upgraded project SWIG version to 3.0.5.

--- Build/Tools/SwigConfig.sh
+++ Build/Tools/SwigConfig.sh
@@ -8,12 +8,10 @@
 # Run this instead of "./configure" from your untarred
 # "swig-<x.x>" build directory.  Then run "make" and "make check"
 # (with the same $PATH set during configuration); if successful,
-# install with "sudo make install" as an administrator.
+# install with "sudo make install" as an administrator.  [You may
+# need to use "sudo" for all steps on some OS versions.]
 #
 # Note the choice of /opt below as the location of dependencies.
-# If you don't have these installed, try to disable them using
-# options like "--without-boost" and "--without-pcre" (though
-# this configuration has not been tested).
 #
 # Kevin Grant (kmg@mac.com)
 # July 29, 2006
@@ -21,7 +19,7 @@
 # IMPORTANT: MacTerm's build system assumes /opt/swig
 # regardless of what you may customize here.  See the
 # Xcode configuration files for a SWIG_PREFIX variable.
-swigversion=2.0.11
+swigversion=3.0.5
 prefix_with_ver=/opt/swig-${swigversion}
 prefix_no_ver=/opt/swig
 
@@ -32,12 +30,13 @@ ln -snf ${prefix_with_ver} ${prefix_no_ver}
 # enable only Python
 ./configure \
 --prefix=${prefix_with_ver} \
---with-boost=/opt/boost \
---with-pcre-prefix=/opt/pcre \
+--with-boost=no \
+--without-pcre \
 --disable-ccache \
---with-python \
+--with-python=/System/Library/Frameworks/Python.framework/Versions/2.6/bin/python \
 --without-allegrocl \
 --without-android \
+--without-cffi \
 --without-chicken \
 --without-clisp \
 --without-csharp \
@@ -46,15 +45,18 @@ ln -snf ${prefix_with_ver} ${prefix_no_ver}
 --without-go \
 --without-guile \
 --without-java \
+--without-javascript \
 --without-lua \
 --without-mzscheme \
 --without-ocaml \
 --without-octave \
 --without-perl5 \
---without-pike \
 --without-php \
+--without-pike \
 --without-r \
 --without-ruby \
+--without-scilab \
 --without-tcl \
+--without-uffi \
 ;
 
