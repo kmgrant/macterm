ee34c3ba918ca8f31608205fceb9833d6ceec083
Kevin Grant <kmg@mac.com>
2018-09-26 17:25:44 -0700

    Change “updates” URL, add latest macOS to API.
    Since the code is now “5.0 alpha”, the default URL for the
    “Check for Updates…” command is once again the daily build.
    The Python API now recognizes the name "Mojave" and will
    not complain at startup when that macOS version is running.

--- Build/Application/PythonCode/pymacterm/utilities.py
+++ Build/Application/PythonCode/pymacterm/utilities.py
@@ -112,8 +112,8 @@ def mac_os_name():
     standard way, because (for instance) platform.mac_version()
     fails with the Panther version of Python.
 
-    >>> mac_os_name() in ['High Sierra', 'Sierra', 'El Capitán', 'Yosemite',
-    ...                   'Mavericks', 'Mountain Lion', 'Lion',
+    >>> mac_os_name() in ['Mojave', 'High Sierra', 'Sierra', 'El Capitán',
+    ...                   'Yosemite', 'Mavericks', 'Mountain Lion', 'Lion',
     ...                   'Snow Leopard', 'Leopard', 'Tiger', 'Panther']
     True
 
@@ -144,6 +144,8 @@ def mac_os_name():
         result = "Sierra"
     elif darwin_version.startswith('17.'):
         result = "High Sierra"
+    elif darwin_version.startswith('18.'):
+        result = "Mojave"
     return result
 
 def slash_free_path(path):
