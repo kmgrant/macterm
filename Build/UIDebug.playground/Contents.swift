783d693814f52b5f9142f686c562e35be9da0340
Kevin Grant <kmg@mac.com>
2021-06-19 15:58:09 -0700

    Add missing previews to UIDebug playground.

--- Build/UIDebug.playground/Contents.swift
+++ Build/UIDebug.playground/Contents.swift
@@ -6,19 +6,22 @@ import MacTermQuills
 // (comment-in whichever UI you want to test)
 //let view = UIAddressList_Previews.previews
 //let view = UIArrangeWindow_Previews.previews
+//let view = UIClipboard_Previews.previews
 //let view = UIDebugInterface_Previews.previews
 //let view = UIKeypads_Previews.previews
 //let view = UIPrefsGeneralOptions_Previews.previews
 //let view = UIPrefsGeneralSpecial_Previews.previews
 //let view = UIPrefsGeneralFullScreen_Previews.previews
 //let view = UIPrefsGeneralNotifications_Previews.previews
-let view = UIPrefsSessionDataFlow_Previews.previews
+//let view = UIPrefsMacroEditor_Previews.previews
+//let view = UIPrefsSessionDataFlow_Previews.previews
 //let view = UIPrefsSessionKeyboard_Previews.previews
 //let view = UIPrefsSessionGraphics_Previews.previews
 //let view = UIPrefsTerminalOptions_Previews.previews
 //let view = UIPrefsTerminalEmulation_Previews.previews
 //let view = UIPrefsTerminalScreen_Previews.previews
 //let view = UIPrefsWorkspaceOptions_Previews.previews
+let view = UIServerBrowser_Previews.previews
 
 NSSetUncaughtExceptionHandler { exception in
 	print("EXCEPTION: \(exception)")
